68f1bd9cf57368796d435502dea97cecbf810c83
Benwagrez <benwagrez@gmail.com>
2019-02-21 10:22:18 -0500

    Base commit

---
+++ footer.php
@@ -0,0 +1,33 @@
+<div class="flexContainer" style="margin: 0; padding: 0; position: relative; top: 0;">
+    <div class="flexItemStart" id="contactUsFlexItem">
+        <h3 style = "text-align: left">TEST</h3>
+        <?php
+            //Get president's information
+            $sql = "SELECT * FROM students WHERE Position = :pos LIMIT 1";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['pos' => 'President']);
+            $presidentData = $stmt->fetchAll();
+            if(!empty($presidentData)){
+                echo '<p>', $presidentData[0][1], ' ', $presidentData[0][2], ': ', $presidentData[0][3], ' (President)</p>';
+            }
+            
+            //Get advisors' information
+            $sql = "SELECT * FROM students WHERE Position = :pos";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['pos' => 'Advisor']);
+            $advisorData = $stmt->fetchAll();
+            for($i = 0; $i<count($advisorData); $i++){
+                echo '<p>', $advisorData[$i][1], ' ', $advisorData[$i][2], ': ', $advisorData[$i][3], ' (Advisor)</p>';
+            }
+        ?>
+        <p>TEST</p>
+    </div>
+    <div class="flexItemStart" id="bugFlexItem">
+        <h3 style = "text-align: left;">TEST</h3>
+        <p>TEST</p>
+        <p>TEST<a href="howToUse.php" style="color: white">TEST</a></p><!--By using the site and reporting errors and/or bugs, you are giving experience to the current computer science students at Lake Park.</p>-->
+    </div>
+    <div class="flexItemCenter" id="imageFlexItem">
+        <img src = "img/mountain.png" id="NHSimageItem" style = "width: 137px; display: block; margin: 0 auto;" >
+    </div>
+</div>
---
+++ header.php
@@ -0,0 +1,65 @@
+<!--This is PHP to be implemented in all pages-->
+
+<div id = "banner" style = "width:100%">
+
+	<img id = "LPLogo" src = "img/LP_logo.png" style = "opacity: 0; height: 100px;"><!--LPHS Logo-->
+        
+	<h1 class = "baseText" style = "padding-bottom: 0px; margin-bottom:0px; color: #005da3;"><span id = "LPNHS" style = "cursor: pointer;" onclick = "location.href='index.php'" title = "LPNHS - Home">TEST</span></h1>
+	<h2 class = "baseTextb" style = "padding-top: 0px; margin-top:0px; color: #666;">TEST</h2>
+	<?php if(isset($_SESSION["StudentID"])) : ?><!--Checking if user is logged in for either sign in or sign out button-->
+		<div id = "headerLogout" class = "headerSignIn"><button id = "headerLogoutButton">Sign Out</button></div>
+	<?php else: ?>
+		<div id = "headerLogin" class = "headerSignIn"><button id = "headerLoginButton">Sign In</button></div>
+	<?php endif; ?>
+
+</div>
+    <script>/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function navBarDD() {
+    document.getElementById("navBarDD").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}</script>
+<div id = "navBarWrapper">
+    <nav id = "navBar" class = "topnav">
+        <?php if(isset($_SESSION["StudentID"])) : ?><!--Checking if student is logged in for different nav bar-->
+            <div id = "navBarNormal"><a class = "baseText" id = "homeLink" href = "index.php">1</a><!--Removing spacing
+            --><a class = "baseText" id = "eventsLink" href = "events.php">2</a><!--Removing spacing
+            --><a class = "baseText" id = "membersLink" href = "members.php">3</a><!--Removing spacing
+			--><a class = "baseText" id = "myProfileLink" href = "my-profile.php">4</a></div><!--Removing spacing
+            --><div class="dropdown"><!--Removing spacing
+            --><a class = "baseText" href="index.php">1</a><!--Removing spacing
+            --><a class = "baseText" href="my-profile.php">5</a><!--Removing spacing            
+            --><button onclick="navBarDD()" class="dropbtn" aria-haspopup="true">&#9776;</button>
+                <div id="navBarDD" class="dropdown-content">
+                    <a style="padding-left: 10px;border-bottom: .5px solid gray;width:100%;" href="events.php">2</a>
+                    <a style="padding-left: 10px;border-bottom: .5px solid gray;width:100%;" href="members.php">3</a>
+                    <a style="padding-left: 10px;border-bottom: .5px solid gray;width:100%;" href="logout.php">Sign Out</a>
+                </div></div>
+        <?php else :?><!--If not logged in then the nav bar below-->
+        <div id = "navBarNormal"><a class = "baseText" id = "homeLink" style="width:33.33%" href = "index.php">1</a><!--Removing spacing
+            --><a class = "baseText" id = "communityInvolvementLink" style="width:33.33%" href = "community-involvement.php">2</a><!--Removing spacing
+			--><a class = "baseText" id = "whatItTakesLink" style="width:33.33%" href = "what-it-takes.php">6</a></div><!--Removing spacing
+            --><div class="dropdown"><a style="width:33.33%" class = "baseText" href="index.php">1</a><!--Removing spacing
+            --><a class = "baseText" style="width:33.33%" href="login.php">Sign In</a><!--Removing spacing
+            --><button onclick="navBarDD()" class="dropbtn" aria-haspopup="true">&#9776;</button>
+                <div id="navBarDD" class="dropdown-content">
+                    <a style="padding-left: 10px;border-bottom: .5px solid gray;width:100%;" href="community-involvement.php">2</a>
+                    <a style="padding-left: 10px;border-bottom: .5px solid gray;width:100%;" href="what-it-takes.php">3</a>
+                </div></div>
+        <?php endif; ?>
+    </nav>
+</div>
