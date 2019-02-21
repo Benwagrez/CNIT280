da3054ab49abb70532bd512054e372d572928b3f
DarkSplash <47859189+DarkSplash@users.noreply.github.com>
2019-02-21 11:16:47 -0500

    changed variable names in footer

--- footer.php
+++ footer.php
@@ -24,8 +24,8 @@
     </div>
     <div class="flexItemStart" id="bugFlexItem">
         <h3 style = "text-align: left;">TEST</h3>
-        <p>TEST</p>
-        <p>TEST<a href="howToUse.php" style="color: white">TEST</a></p><!--By using the site and reporting errors and/or bugs, you are giving experience to the current computer science students at Lake Park.</p>-->
+        <p>Bruh</p>
+        <p>Bruh<a href="customerreturnbarcode.php" style="color: white">Bruh</a></p><!--By using the site and reporting errors and/or bugs, you are giving experience to the current computer science students at Lake Park.</p>-->
     </div>
     <div class="flexItemCenter" id="imageFlexItem">
         <img src = "img/mountain.png" id="NHSimageItem" style = "width: 137px; display: block; margin: 0 auto;" >
