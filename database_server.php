de7d1bd2fbe9da0103e1749b3b37639673d15343
DarkSplash <47859189+DarkSplash@users.noreply.github.com>
2019-02-21 20:10:13 -0600

    Added speicific website database.php file

---
+++ database_server.php
@@ -0,0 +1,11 @@
+<?php
+
+    $host = "localhost";
+    $user = "amswin";
+    $dbname = "amswin_fastfit";
+
+    $dsn = "mysql:host=".$host."; dbname=".$dbname;
+
+    $pdo = new PDO($dsn,$user,"rT^RD2D$48ys");
+
+?>
