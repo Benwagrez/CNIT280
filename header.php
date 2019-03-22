463f0d5b5847fcb54f075043223ea7103b0d4246
DarkSplash <47859189+DarkSplash@users.noreply.github.com>
2019-03-21 22:08:38 -0400

    Cheaky change so admin dashboard isnt avalible w/o login

--- header.php
+++ header.php
@@ -51,7 +51,7 @@ window.onclick = function(event) {
         <?php else :?><!--If not logged in then the nav bar below-->
         <div id = "navBarNormal"><a class = "baseText" id = "homeLink" style="width:33.33%" href = "index.php">Home</a><!--Removing spacing
             --><a class = "baseText" id = "customerReturns" style="width:33.33%" href = "customerreturnbarcode.php">Customer Returns</a><!--Removing spacing
-			--><a class = "baseText" id = "manage" style="width:33.33%" href = "profile.php">Profile</a></div><!--Removing spacing
+			--><a class = "baseText" id = "manage" style="width:33.33%" href = "index.php"></a></div><!--Removing spacing
             --><div class="dropdown"><a style="width:33.33%" class = "baseText" href="index.php">Home</a><!--Removing spacing
             --><a class = "baseText" style="width:33.33%" href="login.php">Sign In</a><!--Removing spacing
             --><button onclick="navBarDD()" class="dropbtn" aria-haspopup="true">&#9776;</button>
